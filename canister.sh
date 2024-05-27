6ca9b8275405e4be89dbb5fa5baa9bd881f3d88a
jesssekeogh <jesse.keogh74@gmail.com>
2024-05-27 19:15:01 +0100

    project setup

---
+++ canister.sh
@@ -0,0 +1,5 @@
+# helpful launch commands
+
+# dfx canister --network ic install --mode reinstall neuronpool_frontend
+# dfx deploy --network ic neuronpool_frontend
+# dfx canister --network ic status neuronpool_frontend
---
+++ src/neuronpool_frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+const App = () => {
+  return <Text color="black">hello</Text>;
+};
+
+export default App;
